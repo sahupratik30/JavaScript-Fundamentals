906427775db5176d5f6600d5ad2d05e586853f73
sahupratik30 <sahupratik30@gmail.com>
2022-10-13 14:09:04 +0530

    initial

---
+++ 10. Flatten array in JavaScript/flatten.js
@@ -0,0 +1,20 @@
+// Flattening an array means to convert a nested array into a single array.
+
+let array = [[[[[0]], [1]], [[[2], [3]]], [[4], [5]]]];
+
+// We need to create a custom flatten() method which will receive an array as a parameter and returns a flattened array i.e, [0, 1, 2, 3, 4, 5] in the above case.
+
+// Implementation:-
+function flatten(arr) {
+  let result = [];
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      result.push(...flatten(item));
+    } else {
+      result.push(item);
+    }
+  });
+  return result;
+}
+
+console.log(flatten(array));
