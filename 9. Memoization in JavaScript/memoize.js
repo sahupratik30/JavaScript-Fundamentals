3f834a78d79e9eff2ba8a077f45ce83c1077ac01
sahupratik30 <sahupratik30@gmail.com>
2022-10-13 12:15:21 +0530

    initial

--- 9. Memoization in JavaScript/memoize.js
+++ 9. Memoization in JavaScript/memoize.js
@@ -4,7 +4,7 @@ Memoization: Memoization is a technique for speeding up applications by caching 
 Let us try to understand this by breaking the definition into small parts.
 
 
-Expensive Function Calls: Time and memory are the two most important resources in computer applications. As a result, an expensive function call is one that consumes large amounts of these two resources due to extensive calculation during execution.
+Expensive Function Calls: Functions that take a lot of time and memory to execute.
 
 Cache: A cache is just a temporary data store that stores data in order to serve future requests for that data more quickly.
 */
